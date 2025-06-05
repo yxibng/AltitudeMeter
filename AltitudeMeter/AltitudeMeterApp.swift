bbcac5b90b39ff90126b722a0420aa5a1681742c
bill <bill@socrates.com>
2025-06-05 16:01:47 +0800

    Initial Commit

---
+++ AltitudeMeter/AltitudeMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AltitudeMeterApp.swift
+//  AltitudeMeter
+//
+//  Created by yxibng on 2025/6/5.
+//
+
+import SwiftUI
+
+@main
+struct AltitudeMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AltitudeMeter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AltitudeMeter
+//
+//  Created by yxibng on 2025/6/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
